d94ae494e45a6ab01af1108bc903b8f9905ee398
jkyuuhub <quswhdrb@naver.com>
2021-11-28 01:40:57 +0900

    Step01 사칙연산

--- Baekjoon/Step01.cpp
+++ Baekjoon/Step01.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
 
+#include <iostream>
+
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!" << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"\^\"\`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+    cout << "-----------------------" << endl;
+
+    int A;
+    int B;
+
+    cout << "A+B" << endl;
+    cin >> A >> B;
+    cout << A + B << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "A-B" << endl;
+    cin >> A >> B;
+    cout << A - B << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "A*B" << endl;
+    cin >> A >> B;
+    cout << A * B << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "A/B" << endl;
+    cin >> A >> B;
+    cout.precision(10);
+    cout << A / (double)B << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "A+B / A-B / A*B / A/B / A%B" << endl;
+    cin >> A >> B;
+    cout << A + B << endl;
+    cout << A - B << endl;
+    cout << A * B << endl;
+    cout << A / B << endl;
+    cout << A % B << endl;
+    cout << "-----------------------" << endl;
+
+
+
 }
