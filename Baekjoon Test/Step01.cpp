35418c7d96f4b1e88a27cc2f6722c68ff2be014c
jkyuuhub <quswhdrb@naver.com>
2021-12-08 19:33:37 +0900

    update

---
+++ Baekjoon Test/Step01.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"\^\"\`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+    cout << "-----------------------" << endl;
+
+    int A;
+    int B;
+    int C;
+    int D;
+
+    cout << "A+B" << endl;
+    cin >> A >> B;
+    cout << A + B << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "A-B" << endl;
+    cin >> A >> B;
+    cout << A - B << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "A*B" << endl;
+    cin >> A >> B;
+    cout << A * B << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "A/B" << endl;
+    cin >> A >> B;
+    cout.precision(10);
+    cout << A / (double)B << endl;
+    cout << "-----------------------" << endl;
+
+    cout << "A+B / A-B / A*B / A/B / A%B" << endl;
+    cin >> A >> B;
+    cout << A + B << endl;
+    cout << A - B << endl;
+    cout << A * B << endl;
+    cout << A / B << endl;
+    cout << A % B << endl;
+    cout << "-----------------------" << endl;
+
+
+    cout << "(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?" << endl;
+    cout << "(A*B)%C는 ((A%C) × (B%C))%C 와 같을까?" << endl;
+    cin >> A >> B >> C;
+    cout << (A + B) % C << endl;
+    cout << ((A % C) + (B % C)) % C << endl;
+    cout << (A * B) % C << endl;
+    cout << ((A % C) * (B % C)) % C << endl;
+    cout << "-----------------------" << endl;
+
+    /*cout << "A * BCD" << endl;
+    cin >> A;
+    cin >> B >> C >> D;
+    if (100 <= A <= 999 && 1 <= B <= 9 && 0 <= C, D <= 9)
+    {
+        cout << A * D << endl;
+        cout << A * C * 10 << endl;
+        cout << A * B * 100 << endl;
+        int E = A * D;
+        int F = A * C * 10;
+        int G = A * B * 100;
+        cout << E + F + G << endl;
+    }
+    else
+    {
+        cout << "재입력" << endl;
+    }*/
+
+}
