34fcf3259baac348e361a4e21556844fc8db7096
jkyuuhub <quswhdrb@naver.com>
2021-12-28 21:36:53 +0900

    Q2 update

--- Baekjoon Test/Step02.if/Step02(1).cpp
+++ Baekjoon Test/Step02.if/Step02(1).cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout << "두 수 비교하기" << endl;
+	// 1) 두 수 비교하기
 
 	int A;
 	int B;
@@ -22,5 +22,33 @@ int main()
 	else
 	{
 		cout << "==" << endl;
+	}
+
+
+	// 2) 점수 별 시험 성적 산출
+
+	int A = 0;
+
+	cin >> A;
+
+	if (90 <= A && A <= 100)
+	{
+		cout << "A" << endl;
+	}
+	else if (80 <= A)  // (80 <= A && A <= 89)
+	{
+		cout << "B" << endl;
+	}
+	else if (70 <= A) // (70 <= A && A <= 79)
+	{
+		cout << "C" << endl;
+	}
+	else if (60 <= A) // (60 <= A && A <= 69)
+	{
+		cout << "D" << endl;
+	}
+	else
+	{
+		cout << "F" << endl;
 	}
 }
