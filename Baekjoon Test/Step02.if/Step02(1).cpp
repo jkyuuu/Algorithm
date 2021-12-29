f9095d59b119de393c129d2fdff728a2a3827ea2
jkyuuhub <quswhdrb@naver.com>
2021-12-29 13:13:51 +0900

    Q3 update

--- Baekjoon Test/Step02.if/Step02(1).cpp
+++ Baekjoon Test/Step02.if/Step02(1).cpp
@@ -51,4 +51,33 @@ int main()
 	{
 		cout << "F" << endl;
 	}
+
+
+	// 3) À±³â
+
+	int a;
+
+	cin >> a;
+
+	if (a % 4 == 0)
+	{
+		if (a % 400 == 0)
+		{
+			cout << "1" << endl;
+		}
+		else if (a % 100 != 0)
+		{
+			cout << "1" << endl;
+		}
+		else
+		{
+			cout << "0" << endl;
+		}
+	}
+	else
+	{
+		cout << "0" << endl;
+	}
+
+
 }
